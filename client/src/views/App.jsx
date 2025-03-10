28ba0da678ce8c3a0971cb8c52fe50e7e8dc0842
Nandana <nandanasheri@gmail.com>
2025-03-10 13:12:32 -0500

    tailwind set up

---
+++ client/src/views/App.jsx
@@ -0,0 +1,12 @@
+
+function App() {
+
+  return (
+    <div className='bg-red-500 flex'>
+      Hello
+
+    </div>
+  )
+}
+
+export default App
--- server/app/example.py
+++ server/app/example.py
@@ -1,4 +1,5 @@
 import requests
 import json 
-res = requests.get('https://cat-fact.herokuapp.com/facts/random?animal_type=dog&amount1')
+res = requests.get('API ENDPOINT')
 response = json.loads(res.text)
+print(response)
