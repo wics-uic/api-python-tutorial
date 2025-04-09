ae59b13090c47ff6a98103adface152ee631682c
Nandana <nandanasheri@gmail.com>
2025-04-09 06:21:25 -0500

    quick fix

--- client/src/views/WiCSTrivia.jsx
+++ client/src/views/WiCSTrivia.jsx
@@ -22,10 +22,15 @@ export default function WiCSTrivia() {
         return (
             <div className='w-screen min-h-screen bg-top-left flex flex-col items-center pt-8 gap-4 font-serif' style={{ backgroundImage: `url(${background})`}}>
                 <h1 className='bg-gray-400 opacity-70 p-5 rounded-2xl text-3xl font-serif font-semibold'>Questions from our Own API</h1>
-                <div className='bg-gray-300 opacity-80 p-8 rounded-2xl flex flex-col gap-4'>
-                    <h2  className='text-xl font-serif font-semibold'>{triviaData ? triviaData['question'] : 'Loading Question'}</h2>
-                    <h2  className='text-l font-serif font-semibold'>{triviaData ? triviaData['answer'] : 'Loading Answer'}</h2>
-                </div>
+                {triviaData && triviaData.map((item) => {
+                    return (
+                        <div className='bg-gray-300 opacity-80 p-8 rounded-2xl flex flex-col gap-4' key={item.id}>
+                            <h2  className='text-xl font-serif font-semibold'>{triviaData ? item['question'] : 'Loading Question'}</h2>
+                            <h2  className='text-l font-serif font-semibold'>{triviaData ? item['answer'] : 'Loading Answer'}</h2>
+                        </div>
+                    )
+                })}
+                
             </div>
         )
 }
